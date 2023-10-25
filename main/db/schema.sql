7b246db065d094f0198c2c4c7ecd6e488804b561
Glidingdiplomat <yankeefan1246@gmail.com>
2023-10-24 21:03:43 -0400

    updated schema file
    adding tables and section as seen in the code.

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -5,9 +5,23 @@ use employee_db
 
 CREATE TABLE department {
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
+    dept_name VARCHAR(30) UNIQUE NOT NULL
 };
 
 CREATE TABLE role {
-    id INT UNSIGNED
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL UNIQUE NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    INDEX
+}
+
+CREATE TABLE employee {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    manager_id INT UNSIGNED,
+    INDEX role_id (role_id),
+    INDEX ma
 }
